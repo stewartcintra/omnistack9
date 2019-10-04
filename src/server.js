d36382a9857e4bfc39a518317245a86eea6592c2
Stewart Cintra <stewartcintra@hotmail.com>
2019-10-04 17:51:33 -0300

    iniciado socket.io

--- src/server.js
+++ src/server.js
@@ -3,14 +3,36 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = server.socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-zajqa.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+const connectedUsers = {};
+io.on('connection', socket => {
+    console.log('Usuario conectado:', socket.id);
+    console.log(socket.handShake.query);
+
+    const { user_id } = socket.handShake.query;
+    connectedUsers[user_id] = socket.id;
+    //socket.emit('hello', 'word');
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
 
 //app.use(cors({ origin: 'htttp://localhost:3333' }))
 app.use(cors());
@@ -18,4 +40,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
