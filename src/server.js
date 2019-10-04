eee7c6db9f296cad4d8d6a6cb4b894e300ce2cd6
Stewart Cintra <stewartcintra@hotmail.com>
2019-10-04 10:26:44 -0300

    add path e rota files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -14,6 +15,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-zajqa.mongodb.net
 //app.use(cors({ origin: 'htttp://localhost:3333' }))
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
